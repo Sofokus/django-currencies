70f59bd22a01c55c7870148b8a125459e46a42db
Lauri Junkkari <latenssi@gmail.com>
2013-06-12 09:37:40 +0300

    Allow the context processor to be enabled while no currencies are defined.

--- currencies/context_processors.py
+++ currencies/context_processors.py
@@ -5,7 +5,10 @@ def currencies(request):
     currencies = Currency.objects.all()
 
     if not request.session.get('currency'):
-        request.session['currency'] = Currency.objects.get(is_default__exact=True)
+        try:
+            request.session['currency'] = Currency.objects.get(is_default__exact=True)
+        except Currency.DoesNotExist:
+            request.session['currency'] = None
 
     return {
         'CURRENCIES': currencies,
